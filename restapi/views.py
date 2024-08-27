63f7f48f9e809c808d0cf715a6617d0faf3684e0
Nicolás Longhi <ndlonghi@gmail.com>
2024-08-27 20:03:41 +0200

    Add API to create and get expenses

--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,32 @@
+from unicodedata import category
+
+from django.forms import model_to_dict
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from restapi import models
+
 
 # Create your views here.
+
+
+class ExpenseListCreate(APIView):
+    def get(self, request):
+        expenses = models.Expense.objects.all()
+
+        all_expenses = [model_to_dict(expense) for expense in expenses]
+
+        return Response(all_expenses, status=200)
+
+    def post(self, request):
+        amount = request.data["amount"]
+        merchant = request.data["merchant"]
+        description = request.data["description"]
+        category = request.data["category"]
+
+        expense = models.Expense.objects.create(
+            amount=amount, merchant=merchant, description=description, category=category
+        )
+
+        return Response(model_to_dict(expense), status=201)
